6554505d885a78135fc71faebba39784eb75ed21
13th <pavel.voronin@ligastavok.ru>
2025-02-04 17:14:30 +0300

    Проверили ДЗ

--- ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
@@ -2,15 +2,16 @@ package ru.ls.qa.school.core;
 
 public class Point {
 
-    public Point(double x, double y){
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
     private final double x;
     private final double y;
-    
-    public double distance(Point point){
-        return Math.sqrt(((this.x-point.x)*(this.x-point.x))
-                +((point.y-this.y)*(point.y-this.y)));
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow((this.x - point.x), 2)
+                + Math.pow((point.y - this.y), 2));
     }
 }
--- ls-java-core/src/main/java/ru/ls/qa/school/core/PointTask.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/PointTask.java
@@ -3,7 +3,7 @@ package ru.ls.qa.school.core;
 public class PointTask {
     public static void main(String[] args) {
         Point pointA = new Point(1, 2);
-        Point pointB = new Point(2, 3);
+        Point pointB = new Point(5, 5);
         System.out.println("Check the answer: " + pointA.distance(pointB));
     }
 }
