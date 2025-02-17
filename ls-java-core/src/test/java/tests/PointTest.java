bf478f28a59edb9f5af7009a1a6a69911a49ec83
lyudmila.mordvinova <lyudmila.mordvinova@simbirsoft.com>
2025-02-17 21:29:13 +0400

    done 3rd homework

--- ls-java-core/src/test/java/tests/PointTest.java
+++ ls-java-core/src/test/java/tests/PointTest.java
@@ -1,19 +1,24 @@
 package tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.ls.qa.school.core.Point;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PointTest {
-    int x, y, x1, y1;
-    Point point = new Point(x, y);
-    Point point2 = new Point(x1, y1);
 
+    private static Stream<Arguments> oddIntSource() {
+        return Stream.of(
+                Arguments.of(1, 2, 5, 5 ,5)
+        );
+    }
     @ParameterizedTest
-    @ValueSource(ints = {1, 3, 5, -3, 15})
-    public void isOddTest(int number){
-        assertEquals(point.distance(point2), true);
+    @MethodSource("oddIntSource")
+    public void isOddTest(int x1, int y1, int x2, int y2, int result){
+        assertEquals(new Point(x1, y1).distance(new Point(x2, y2)), result);
     }
 }
