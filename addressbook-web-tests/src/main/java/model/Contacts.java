3806468ebc166de546880488a7bfd624f455a2a3
lyudmila.mordvinova <lyudmila.mordvinova@simbirsoft.com>
2025-05-26 21:40:17 +0400

    Удаление и обновление контакта: тесты написаны

--- addressbook-web-tests/src/main/java/model/Contacts.java
+++ addressbook-web-tests/src/main/java/model/Contacts.java
@@ -30,12 +30,6 @@ public class Contacts extends ForwardingSet<ContactData> {
         return delegate;
     }
 
-    public Contacts withAdded(ContactData contact) {
-        Contacts contacts = new Contacts(this);
-        contacts.add(contact);
-        return contacts;
-    }
-
     public Contacts without(ContactData contact) {
         Contacts contacts = new Contacts(this);
         contacts.remove(contact);
